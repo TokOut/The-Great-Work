9527d671cc9b9d7e06c88816d5cbfdcacf50ec8e
Anatoly <weinstein.anatoly@gmail.com>
2015-11-21 16:40:41 +0100

    This is a class for using an Integer line with Y, X1, X2

---
+++ IntegerLine.lua
@@ -0,0 +1,46 @@
+IntegerParameter = class()
+
+function IntegerParameter:init(x1, y, x2, cp)
+    self.x1 = x1
+    self.y = y
+    self.x2 = x2
+    self.ellipseposition = cp
+    if self.ellipseposition > x2 or self.ellipseposition < x1 or self.ellipseposition == x2 then
+        self.ellipseposition = x1
+    end
+    self.value = self.ellipseposition - self.x1
+end
+
+function IntegerParameter:draw()
+    strokeWidth(10)
+    stroke(255, 255, 255, 255)
+    line(self.x1, self.y, self.x2, self.y)
+    
+    fill(225, 221, 192, 255)
+    strokeWidth(0)
+    ellipse(self.ellipseposition, self.y, 25)
+end
+
+function IntegerParameter:touched(touch)
+    if touch.state == BEGAN and self:hit(vec2(touch.x, touch.y)) then
+        print("Touched")
+    end
+    if touch.state == MOVING and self:hit(vec2(touch.x, touch.y)) then
+        print("Moving to", CurrentTouch.x)
+        self.ellipseposition = CurrentTouch.x
+        print("Have been Moved!")
+        cp = self.ellipseposition
+        print("Recognized values!")
+    end
+end
+
+function IntegerParameter:hit(p)
+    local r = self.x2
+    local l = self.x1
+    local t = self.y + 5
+    local b = self.y - 5
+    if p.x > l and p.x < r and p.y > b and p.y < t then
+        return true
+    end
+    return false
+end
